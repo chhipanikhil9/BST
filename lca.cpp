f663aa337965a1088f0ed574c28c069af07904eb
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-24 12:59:25 +0530

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,13 @@
+// if both are left go left, else if both are right go right, else just return the root because this root is 
+// partitioning both node in left and right and this is BST so this root gonna be the LCA;
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+  if(root==NULL) return root;
+  if(root->val>p->val and root->val>q->val){
+      return lowestCommonAncestor(root->left,p,q);
+  }
+  else if(root->val < p->val and root->val < q->val){
+      return lowestCommonAncestor(root->right,p,q);
+  }
+  else return root;
+}
