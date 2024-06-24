ee17df5c817e63aac8b8586668d8ca3841ce1890
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-24 13:04:03 +0530

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -1,5 +1,7 @@
 // if both are left go left, else if both are right go right, else just return the root because this root is 
 // partitioning both node in left and right and this is BST so this root gonna be the LCA;
+// Time: O(H)
+// space: O(H) recursion
 
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
   if(root==NULL) return root;
